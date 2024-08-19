c0a98d4bcc2a1c46d88084cc5c847af2a9aefbe5
rYmS26 <dyingforme1@gmail.com>
2024-08-19 13:04:31 +0700

    add

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,7 +47,7 @@
 	<meta name="author" content="Kodinger">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>My Login Page</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/css/bootstrap.min.css" integrity="sha384-D4IoOf6b8VTEo5KNt1FnFlIHGIVGI5l2wPYi6Y+3X/bBu1g9DXvS4iyE3iItHMjs" crossorigin="anonymous">
 	<style>
         html,body {
 	height: 100%;
@@ -189,9 +189,9 @@ body.my-login-page {
 		</div>
 	</section>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha384-SgWQX71pbKZ07bMycZTAwQFcxB6JCMzhVDgMDop3HYGPGb5xpR3apEy99Dx86uYH" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-DAsBR0AHcfe/V5zA3FdvYX3t8RoGaTy7CENrq9l5Al32rdU6OTc0AOMmUsZzsjZn" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-h4MUkkV3ZNUcAE28zTX5QJQ3ng/ABXHhzHQ7JS5wWCDuzKT2Bz7X0GQUbS9fEYYx" crossorigin="anonymous"></script>
 	<script src="js/my-login.js"></script>
 </body>
 </html>
